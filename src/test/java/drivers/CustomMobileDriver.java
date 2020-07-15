a00e783cc61cbe4f41c9bd4a0bd5813b8deea558
svasenkov <aanher@gmail.com>
2020-07-15 21:51:18 +0300

    update bs app

--- src/test/java/drivers/CustomMobileDriver.java
+++ src/test/java/drivers/CustomMobileDriver.java
@@ -20,7 +20,7 @@ public class CustomMobileDriver implements WebDriverProvider {
 
         capabilities.setCapability("device", "Google Pixel 3");
         capabilities.setCapability("os_version", "9.0");
-        capabilities.setCapability("app", "c700ce60cf13ae8ed97705a55b8e022f13c5827c");
+        capabilities.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
         return new AndroidDriver(getBrowserstackUrl(), capabilities);
     }
